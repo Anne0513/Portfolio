a4172d6394f84367543f22e0d3f03464879ab1db
Tianqi Liu <148938970+Anne0513@users.noreply.github.com>
2025-05-25 00:31:09 +0800

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,7 @@ import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
 import Toolstack from "./Toolstack";
-import laptopImg from "../../Assets/academic.png";
+import academicImg from "../../Assets/academic.png";
 
 function About() {
   return (
