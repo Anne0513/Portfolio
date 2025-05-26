63c353bf76fccbf4cca55a45e97ea5d2b5bee9dd
Tianqi Liu <148938970+Anne0513@users.noreply.github.com>
2025-05-26 09:21:58 +0800

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Tianqi Liu </span>
+            from <span className="purple"> Liaoning, China.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I'm a junior student studying at Southwestern University of Finance and Economics(SWUFE) in Chengdu.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            My major is MIS, which has aroused my interest in coding. I hope to be a data analyst or a product manager in Internet enterprises. I also look forward to working in Shanghai because there're lots of opportunities waiting for me.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> music
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> traveling
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> speech
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Fake it till you make it!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">keep pushing forward.</footer>
         </blockquote>
       </Card.Body>
     </Card>
