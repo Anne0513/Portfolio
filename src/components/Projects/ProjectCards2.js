bf878538b60a0d0a4c9d8c1a416c8e2d73f65103
Tianqi Liu <148938970+Anne0513@users.noreply.github.com>
2025-07-06 08:56:37 +0800

    Update ProjectCards2.js

--- src/components/Projects/ProjectCards2.js
+++ src/components/Projects/ProjectCards2.js
@@ -7,25 +7,35 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards2(props) {
   return (
     <Card className="project-card-view">
+      {/* 图片部分 */}
+      {props.imgPath && (
+        <Card.Img variant="top" src={props.imgPath} alt="Project image" />
+      )}
+
       {/* 如果提供了音频路径，则显示音频播放器 */}
       {props.audioPath && (
-        <audio controls style={{ width: "100%" }}>
+        <audio controls style={{ width: "100%", marginTop: "15px" }}>
           <source src={props.audioPath} type="audio/mpeg" />
           Your browser does not support the audio element.
         </audio>
       )}
 
       <Card.Body>
+        {/* 标题 */}
         <Card.Title>{props.title}</Card.Title>
+
+        {/* 描述文字 */}
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
+
+        {/* GitHub/Blog 链接 */}
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
 
-        {/* 如果有 demoLink 且不是博客，则显示 Demo 按钮 */}
+        {/* Demo 链接 */}
         {!props.isBlog && props.demoLink && (
           <Button
             variant="primary"
@@ -33,8 +43,7 @@ function ProjectCards2(props) {
             target="_blank"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <CgWebsite /> &nbsp; Demo
           </Button>
         )}
       </Card.Body>
